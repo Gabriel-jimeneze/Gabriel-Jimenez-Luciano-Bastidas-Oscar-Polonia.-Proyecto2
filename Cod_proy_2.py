cc9c6772d211b022d58545dd5e93137a7e11a2d5
Lbasper <99780061+Lbasper@users.noreply.github.com>
2022-03-13 09:07:36 -0500

    Errores codificación csv

--- Cod_proy_2.py
+++ Cod_proy_2.py
@@ -7,5 +7,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data_2014=pd.read_csv("Camara 2014.csv", delimiter=";")
+#Datos
+data_2014=pd.read_csv("Camara 2014.csv", delimiter=";",encoding="ISO-8859-1")
 data_2018=pd.read_csv("Camara 2018.csv", delimiter=";")
+Estad_educacion=pd.read_csv("MEN_ESTADISTICAS_EN_EDUCACION_EN_PREESCOLAR__B_SICA_Y_MEDIA_POR_DEPARTAMENTO.csv", delimiter=";")
+
+#Llaves
+llav_data_2014=data_2014.keys
+llav_data_2018=data_2018.keys
+llav_Estad_educacion=Estad_educacion.keys
+
+def pvalue(listA,listB,nombre):
+    
