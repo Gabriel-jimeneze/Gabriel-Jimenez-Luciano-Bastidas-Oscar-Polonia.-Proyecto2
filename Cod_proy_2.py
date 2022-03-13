c05a3c433d6efe774bb0ca8db76276bef591fe84
Lbasper <99780061+Lbasper@users.noreply.github.com>
2022-03-13 08:50:09 -0500

    -

--- Cod_proy_2.py
+++ Cod_proy_2.py
@@ -6,3 +6,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+data_2014=pd.read_csv("Camara 2014.csv", delimiter=";")
+data_2018=pd.read_csv("Camara 2018.csv", delimiter=";")
